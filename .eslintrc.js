e83f5767aad5b50efd04a43c27869f55ed5dcec6
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-12 02:54:50 -0500

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	extends: [
+		'airbnb-base',
 		'airbnb-typescript/base',
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
