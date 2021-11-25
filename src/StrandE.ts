1d92f3846a0008ff2f237265154634acc3a9d16a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-24 23:50:25 -0500

    fix passage select pagination
    border covers top option

--- src/StrandE.ts
+++ src/StrandE.ts
@@ -60,7 +60,7 @@ export class StrandE extends Strand {
 		const passages = Object.keys(this.passages)
 			.filter((i) => !i.match(/\d/))
 			.map((i) => `[[${i}]]`);
-		const pages = chunks(passages, 24);
+		const pages = chunks(passages, 23);
 		pages.forEach((i, idx) => {
 			if (pages.length > 1) {
 				i.push(`[[passage select ${(idx + 1) % pages.length}]]`);
