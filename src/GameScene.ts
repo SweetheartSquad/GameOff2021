6cb19675ecb2f3c947e9b3204002fde13fe14f5c
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-02-05 15:52:15 -0500

    localize interaction prompt

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -169,10 +169,13 @@ export class GameScene {
 			} else {
 				const { passage, label = 'talk', focus, gameObject } = top.plugin;
 				this.interactionFocus = focus ? add(top.position, focus) : top.position;
-				this.dialogue.prompt(`< ${label.toUpperCase()} >`, () => {
-					this.strand.gameObject = gameObject;
-					this.strand.goto(passage);
-				});
+				this.dialogue.prompt(
+					`< ${(this.strand.passages[label]?.body || label).toUpperCase()} >`,
+					() => {
+						this.strand.gameObject = gameObject;
+						this.strand.goto(passage);
+					}
+				);
 			}
 		};
 		Events.on(
